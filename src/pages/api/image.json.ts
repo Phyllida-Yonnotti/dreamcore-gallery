4b50aaeabec5714bbb809afacc1b6432db77120f
Phyllida-Yonnotti <84011324+Phyllida-Yonnotti@users.noreply.github.com>
2026-08-08 16:00:52 +0900

    correct token name

--- src/pages/api/image.json.ts
+++ src/pages/api/image.json.ts
@@ -6,6 +6,9 @@ export const prerender = false;
 
 export const GET: APIRoute = async () => {
   try {
+    // 在这里显式传入 token 参数，使用 import.meta.env 读取你的自定义变量
+    const token = import.meta.env.BLOB_PUBLIC_WEBHOOK_PUBLIC_KEY;
+
     // 获取 thai/ 文件夹下的所有文件
     const { blobs } = await list({ prefix: 'thai/' });
     
