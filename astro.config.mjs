e61cc7cee562a1692ee4eff9d7b02fbc7da02ae7
Phyllida-Yonnotti <84011324+Phyllida-Yonnotti@users.noreply.github.com>
2026-08-16 01:55:01 +0900

    fix astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,20 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from 'astro/config';
 import vercel from '@astrojs/vercel';
 
 export default defineConfig({
-  output: 'server', // 开启 SSR 服务端渲染，支持 API 路由
-  adapter: vercel(),
+  env: {
+    schema: {
+      BLOB_READ_WRITE_TOKEN: envField.string({ 
+        context: 'server', 
+        access: 'secret',
+        optional: true,
+      }),
+    }
+  },
+  vite: {
+    ssr: {
+      external: ['@neondatabase/serverless']
+    }
+  }
 });
