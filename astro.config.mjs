7302638e64d2a7260f12997c462cc9940bcbad45
Phyllida-Yonnotti <84011324+Phyllida-Yonnotti@users.noreply.github.com>
2026-08-16 01:57:50 +0900

    fix astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
-// @ts-check
 import { defineConfig, envField } from 'astro/config';
 import vercel from '@astrojs/vercel';
 
 export default defineConfig({
+  adapter: vercel(),
+
   env: {
     schema: {
-      BLOB_READ_WRITE_TOKEN: envField.string({ 
+      BLOB_PUBLIC_READ_WRITE_TOKEN: envField.string({ 
         context: 'server', 
         access: 'secret',
         optional: true,
